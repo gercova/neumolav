0791860c012339db81b0aba50b872a919e8285d5
gercova <germancotrina17@gmail.com>
2025-10-20 23:15:38 -0500

    fillable foto_representante agregado al modelo Enterprise

--- app/Models/Enterprise.php
+++ app/Models/Enterprise.php
@@ -11,7 +11,31 @@ class Enterprise extends Model {
     use HasFactory, AuditLogTrait;
     protected $table        = 'empresa';
     protected $primaryKey   = 'id';
-    protected $fillable     = ['razon_social', 'nombre_comercial', 'ruc', 'email', 'descripcion', 'frase', 'mision', 'vision', 'ubigeo', 'iframe_location', 'direccion', 'pais', 'codigo_pais', 'telefono', 'pagina_web', 'representante_legal', 'logo', 'logo_mini', 'logo_receta', 'rubro', 'fecha_creacion', 'autocomplete'];
+    protected $fillable     = [
+        'razon_social', 
+        'nombre_comercial', 
+        'ruc', 
+        'email', 
+        'descripcion', 
+        'frase', 
+        'mision', 
+        'vision', 
+        'ubigeo', 
+        'iframe_location', 
+        'direccion', 
+        'pais', 
+        'codigo_pais', 
+        'telefono', 
+        'pagina_web', 
+        'representante_legal',
+        'foto_representante',
+        'logo', 
+        'logo_mini', 
+        'logo_receta', 
+        'rubro', 
+        'fecha_creacion', 
+        'autocomplete'
+    ];
     protected $dates        = ['created_at', 'updated_at'];
     protected $casts        = [
         'id'                => 'integer',
