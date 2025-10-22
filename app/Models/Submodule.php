ad361f20ddceac860a5196ca13932fd8e4392bd1
gercova <germancotrina17@gmail.com>
2025-10-22 09:22:53 -0500

    Campo Nombre agregado al fillable del modelo

--- app/Models/Submodule.php
+++ app/Models/Submodule.php
@@ -12,11 +12,12 @@ class Submodule extends Model
     use HasFactory, SoftDeletes, AuditLogTrait;
     protected $table        = 'sub_module';
     protected $primaryKey   = 'id';
-    protected $fillable     = ['module_id','descripcion', 'detalle', 'icono'];
+    protected $fillable     = ['module_id', 'descripcion', 'nombre', 'detalle', 'icono'];
     protected $dates        = ['created_at', 'updated_at', 'deleted_at'];
     protected $casts        = [
         'module_id'     => 'integer',
         'descripcion'   => 'string',
+        'nombre'        => 'string',
         'detalle'       => 'string',
         'icono'         => 'string',
         'created_at'    => 'datetime',
