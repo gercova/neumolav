def8f758aa9bdc4e03bde1ea86ae938574591603
Gercova <germancotrina17@gmail.com>
2026-01-01 21:26:29 -0500

    feat: implementación de nuevas migraciones (id_historia)

---
+++ database/migrations/2026_01_01_201153_add_id_historia_in_examen_diagnostico_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('examen_diagnostico', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_historia')->nullable()->after('id_examen');
+            $table->foreign('id_historia')->references('id')->on('historias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('examen_diagnostico', function (Blueprint $table) {
+            $table->dropForeign(['id_historia']);
+            $table->dropColumn(['id_historia']);
+        });
+    }
+};
