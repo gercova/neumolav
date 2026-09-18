0abc011e425e46c33ea750b6c0e97ddfbcd5078d
gercova <germancotrina17@gmail.com>
2026-09-17 23:15:04 -0500

    feat: implementación del método de permissions

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use App\Traits\AuditLogTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Permission\Models\Permission;
 
 class Module extends Model
 {
@@ -22,5 +24,9 @@ class Module extends Model
 
     public function submodules(): HasMany {
         return $this->hasMany(Submodule::class, 'module_id', 'id');
+    }
+
+    public function permissions(): BelongsToMany {
+        return $this->belongsToMany(Permission::class, 'module_permission', 'module_id', 'permission_id');
     }
 }
