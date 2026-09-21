9f9697292d5b9aae63f7f77ddc54d1bbadf925c2
gercova <germancotrina17@gmail.com>
2026-09-21 16:44:08 -0500

    cambio 333333

--- app/Http/Requests/AppointmentValidate.php
+++ app/Http/Requests/AppointmentValidate.php
@@ -15,9 +15,9 @@ class AppointmentValidate extends FormRequest {
             'id_historia'               => 'required',
             'dni'                       => 'required',
             'sintomas'                  => 'required|string',
-            'diagnostico'               => 'nullable|string',
+            // 'diagnostico'               => 'nullable|string',
             'plan'                      => 'required|string',
-            //'tratamiento'               => 'required|string',
+            // 'tratamiento'               => 'required|string',
             'recomendaciones'           => 'nullable|string',
         ];
     }
@@ -28,7 +28,7 @@ class AppointmentValidate extends FormRequest {
             'dni.required'              => 'El campo DNI es requerido',
             'sintomas.required'         => 'El campo Síntomas es requerido',
             'plan.required'             => 'El campo Plan es requerido',
-            //'tratamiento.required'      => 'El campo Tratamiento es requerido',
+            // 'tratamiento.required'      => 'El campo Tratamiento es requerido',
         ];
     }
 
@@ -37,9 +37,9 @@ class AppointmentValidate extends FormRequest {
             'id_historia'               => trim(strip_tags($this->id_historia)),
             'dni'                       => trim(strip_tags($this->dni)),
             'sintomas'                  => trim(strip_tags($this->sintomas)),
-            'diagnostico'               => trim(strip_tags($this->diagnostico)),
+            // 'diagnostico'               => trim(strip_tags($this->diagnostico)),
             'plan'                      => trim(strip_tags($this->plan)),
-            //'tratamiento'               => trim(strip_tags($this->tratamiento)),
+            // 'tratamiento'               => trim(strip_tags($this->tratamiento)),
             'recomendaciones'           => trim(strip_tags($this->recomendaciones)),
         ]);
     }
