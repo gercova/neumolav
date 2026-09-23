5e609a434a2cd8c00aad58e206bf9af4daeb0b05
gercova <germancotrina17@gmail.com>
2026-09-22 20:37:00 -0500

    feat: método index modificado

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Appointment;
+use App\Models\AppointmentStatus;
+use App\Models\DocumentType;
 use App\Models\Exam;
 use App\Models\History;
+use App\Models\Sex;
+use App\Models\TipoAtencion;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller {
     
@@ -14,11 +20,28 @@ class HomeController extends Controller {
         $this->middleware(['auth', 'prevent.back']);
     }
 
-    public function index(): View {
-        $historias  = History::whereNull('deleted_at')->count();
-        $examenes   = Exam::whereNull('deleted_at')->count();
-        $controles  = Appointment::whereNull('deleted_at')->count();
-        $users      = User::whereNull('deleted_at')->count();
-        return view('home.index', compact('historias', 'examenes', 'controles', 'users'));
+    public function index(Request $request): View {
+        $historias     = History::whereNull('deleted_at')->count();
+        $examenes      = Exam::whereNull('deleted_at')->count();
+        $controles     = Appointment::whereNull('deleted_at')->count();
+        $users         = User::whereNull('deleted_at')->count();
+
+        $selectedDate  = $request->query('date', Carbon::today()->format('Y-m-d'));
+        $documentTypes = DocumentType::where('id', '!=', 2)->get();
+        $sexes         = Sex::get();
+        $statuses      = AppointmentStatus::all();
+        $tiposAtencion = TipoAtencion::all();
+
+        return view('home.index', compact(
+            'historias',
+            'examenes',
+            'controles',
+            'users',
+            'selectedDate',
+            'documentTypes',
+            'sexes',
+            'statuses',
+            'tiposAtencion'
+        ));
     }
 }
